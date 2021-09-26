affced79b6d87a5545a8836aaf4445a4f8c5d956
daeun <zerozero7bang@gmail.com>
2021-09-26 15:02:25 +0900

    BOJ15685 주석

--- 쌔피맨조/다은/BOJ15685/BOJ15685_ranker.py
+++ 쌔피맨조/다은/BOJ15685/BOJ15685_ranker.py
@@ -1,6 +1,11 @@
+import sys
+from pandas import DataFrame
+sys.stdin = open("input.txt", "r")
+
 a = [[0]*101 for _ in range(101)]
 dr = [[0]]
 
+# 10세대까지, 각 세대에서 점들의 회전 순서를 미리 작성
 for _ in range(10):
     dr.append(dr[-1] + [i+1 for i in dr[-1][::-1]])
 
@@ -9,7 +14,9 @@ dy = [0, -1, 0, 1]
 
 for _ in range(int(input())):
     x, y, d, g = map(int, input().split())
+    # 0 세대 드래곤커브
     a[x][y] = 1
+    # g세대에 도착할 때까지 회전시켜가며 체크
     for k in dr[g]:
         x += dx[(d + k) % 4]
         y += dy[(d + k) % 4]
